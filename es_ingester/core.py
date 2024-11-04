45a8c531d7dd608cd9765df3474e2933617eebc0
avis <avis@aviss-MacBook-Pro.local>
2024-11-05 00:47:34 +0530

    minor changes

--- es_ingester/core.py
+++ es_ingester/core.py
@@ -6,31 +6,60 @@ import sys
 import threading
 import yaml
 from elasticsearch import Elasticsearch
+import time
 
 class ESIngester:
-    def __init__(self, es_host, username, password, index_name):
+    def __init__(self, es_host, username, password, index_name, verbose, parent=None):
         self.es = Elasticsearch([es_host], http_auth=(username, password))
         self.index_name = index_name
+        self.verbose = verbose
+        self.lock = threading.Lock()
+        self.total_documents = 0
+        self.parent_data = {}
+
+        # Parse the parent flag if provided
+        if parent:
+            try:
+                key, value = parent.split(":", 1)
+                self.parent_data[key] = value
+            except ValueError:
+                print("Invalid format for -parent. Use key:value format.")
+                sys.exit(1)
 
     def ingest_jsonl(self, lines):
-        for line in lines:
+        self.total_documents = len(lines)
+        for index, line in enumerate(lines):
             doc = json.loads(line)
+
+            # Add parent data if provided
+            if self.parent_data:
+                doc.update(self.parent_data)
+
             self.es.index(index=self.index_name, document=doc)
+            if self.verbose:
+                self.print_progress(index + 1, self.total_documents)
 
     def ingest_json(self, data_key, json_data):
         documents = self.extract_documents(data_key, json_data)
+        self.total_documents = len(documents)
         threads = []
 
-        for doc in documents:
-            thread = threading.Thread(target=self.index_document, args=(doc,))
+        for index, doc in enumerate(documents):
+            # Add parent data if provided
+            if self.parent_data:
+                doc.update(self.parent_data)
+
+            thread = threading.Thread(target=self.index_document, args=(doc, index))
             threads.append(thread)
             thread.start()
 
         for thread in threads:
             thread.join()
 
-    def index_document(self, doc):
+    def index_document(self, doc, index):
         self.es.index(index=self.index_name, document=doc)
+        if self.verbose:
+            self.print_progress(index + 1, self.total_documents)
 
     def extract_documents(self, data_key, json_data):
         keys = data_key.split("->")
@@ -43,6 +72,10 @@ class ESIngester:
                 documents = documents.get(key)
 
         return documents if isinstance(documents, list) else [documents]
+
+    def print_progress(self, processed, total):
+        with self.lock:
+            print(f"\rProcessed {processed}/{total} documents...", end='', flush=True)
 
 def load_config():
     config_path = os.path.expanduser("~/.es_ingester_config.yaml")
@@ -70,6 +103,8 @@ def main():
     parser.add_argument('-threads', type=int, default=20, help='Number of threads for ingestion')
     parser.add_argument('-json', help='Key for JSON extraction (e.g., "result")')
     parser.add_argument('-jsonl', action='store_true', help='Indicates that stdin contains newline-separated JSON documents')
+    parser.add_argument('-verbose', action='store_true', help='Show progress of document ingestion')
+    parser.add_argument('-parent', help='Add a key-value pair to each document in the format key:value')
 
     args = parser.parse_args()
 
@@ -92,7 +127,8 @@ def main():
     # Save config to YAML file if not already present
     save_config(es_host, username, password)
 
-    ingester = ESIngester(es_host, username, password, args.indexname)
+    # Initialize ingester with parent data if provided
+    ingester = ESIngester(es_host, username, password, args.indexname, args.verbose, parent=args.parent)
 
     # Read from stdin based on the input type specified
     if args.jsonl:
@@ -106,6 +142,9 @@ def main():
     else:
         print("No input specified. Use -json or -jsonl.")
         sys.exit(1)
+
+    if args.verbose:
+        print("\nIngestion complete.")
 
 if __name__ == '__main__':
     main()
